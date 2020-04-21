53bf122fb3ab54e35ec7982634c157976b196231
Brzezik <dawid.bgit383@wp.pl>
2020-04-21 13:16:31 +0200

    Dodano ikonę

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const Koa = require('koa')
 const serve = require('koa-static')
 const logger = require('koa-logger')
-
+const favicon = require('koa-favicon')
 const app = new Koa()
 const port = process.env.PORT || 3000
 
@@ -15,7 +15,7 @@ app.use(userRoutes.routes())
 
 const taskRoutes = require('./routes/tasks')
 app.use(taskRoutes.routes())
-
+app.use(favicon(__dirname + '/client/favicon.ico'))
 app.listen(port)
 
 console.log('App is listening at http://127.0.0.1:3000')
