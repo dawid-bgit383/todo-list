a4f1ee65550edb4e4916a40270067187fd51bd30
Brzezik <dawid.bgit383@wp.pl>
2020-04-25 11:55:48 +0200

    Dodano funkcję wyświetlania listy zadań

--- server/store.js
+++ server/store.js
@@ -18,6 +18,19 @@ const addTask = async ({ title }) => (
     })
   })
 )
+const listTasks = async () => (
+  new Promise((resolve, reject) => {
+    const query = new storage.TableQuery()
+      .select(['title'])
+      .where('PartitionKey eq ?', 'task')
+
+    service.queryEntities(table, query, null, (error, result) => {
+      !error ? resolve(result.entries.map((entry) => ({
+        title: entry.title._
+      }))) : reject()
+    })
+  })
+)
 
 const init = async () => (
   new Promise((resolve, reject) => {
@@ -29,5 +42,6 @@ const init = async () => (
 
 module.exports = {
   init,
-  addTask
+  addTask,
+  listTasks
 }
